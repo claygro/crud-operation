9770758bdd6653d977183185c4af8d3fc52a5f1a
claygro <dhamala80130@gmail.com>
2025-06-16 22:02:56 +0545

    adding dotenv package

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 import mysql2 from "mysql2/promise";
-
+import dotenv from "dotenv";
+dotenv.config();
 export default await mysql2.createConnection({
-  host: "localhost",
-  user: "your user name",
-  password: "your password",
-  database: "your database name",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
